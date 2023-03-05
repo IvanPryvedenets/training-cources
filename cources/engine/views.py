f19a41c266ae7fa2cff09ae9b9d24076674d3ba7
Project <pryvivan@gmail.com>
2023-03-05 19:42:11 +0200

    Created slider and was doing onther staff with static files

--- cources/engine/views.py
+++ cources/engine/views.py
@@ -1,10 +1,9 @@
 from django.views import View
 from django.shortcuts import render
 
-from django.http import HttpResponse
 
 
 # Create your views here.
 class HomePage(View):
     def get(self, request):
-        return HttpResponse('hello world')
+        return render(request, 'engine/home.html')
