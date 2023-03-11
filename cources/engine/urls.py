b5891bd766bb9ba81e4e608b1bd978964c9b40fd
Project <pryvivan@gmail.com>
2023-03-11 23:15:02 +0200

    Created templates and basic views

--- cources/engine/urls.py
+++ cources/engine/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from engine.views import *
 
 urlpatterns = [
-    path('', HomePage.as_view(), name='test'),
+    path('', HomePage.as_view(), name='home'),
+    path('about/', AboutPage.as_view(), name='about'),
+    path('courses/', CoursesPage.as_view(), name='courses'),
+    path('staff/', StaffPage.as_view(), name='staff'),
+    path('blog/', BlogPage.as_view(), name='blog'),
+    path('contact/', ContactPage.as_view(), name='contact'),
 ]
--- cources/engine/views.py
+++ cources/engine/views.py
@@ -7,3 +7,23 @@ from django.shortcuts import render
 class HomePage(View):
     def get(self, request):
         return render(request, 'engine/home.html')
+
+class AboutPage(View):
+    def get(self, request):
+        return render(request, 'engine/about.html')
+
+class CoursesPage(View):
+    def get(self, request):
+        return render(request, 'engine/courses.html')
+
+class StaffPage(View):
+    def get(self, request):
+        return render(request, 'engine/staff.html')
+
+class BlogPage(View):
+    def get(self, request):
+        return render(request, 'engine/blog.html')
+
+class ContactPage(View):
+    def get(self, request):
+        return render(request, 'engine/contact.html')
